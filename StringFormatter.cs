b78b941b8ae374566ed029bbd019b95305afbae5
Riku Wada <riku.wada@vivaenergy.com.au>
2025-07-17 23:55:20 +1000

    pointsbet-test added exceptions and a filter for bad items

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -2,23 +2,26 @@
 
 namespace PointsBet_Backend_Online_Code_Test
 {
-
-    /*
-    Improve a block of code as you see fit in C#.
-    You may make any improvements you see fit, for example:
-      - Cleaning up code
-      - Removing redundancy
-      - Refactoring / simplifying
-      - Fixing typos
-      - Any other light-weight optimisation
-    */
     public class StringFormatter
     {
 
         //Code to improve
         public static string ToCommaSeparatedList(string[] items, string quote)
         {
-            return String.Join(", ", items.Select(item => $"{quote}{item}{quote}"));
+            if (items == null || items.Length == 0)
+            {
+                throw new ArgumentException("items is null or empty");
+            }
+
+            if (quote == null)
+            {
+                throw new ArgumentException("quote is null");
+            }
+
+            return String.Join(", ", items
+                    .Where(item => !String.IsNullOrWhiteSpace(item))
+                    .Select(item => $"{quote}{item}{quote}")
+                );
         }
     }
 }
