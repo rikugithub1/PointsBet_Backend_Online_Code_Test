a5b621207ebf2522d221d2e38668fe7eb2a39f9c
Riku Wada <riku.wada@vivaenergy.com.au>
2025-07-17 23:29:47 +1000

    pointsbet-test improved ToCommaSeparatedList method

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -16,19 +16,9 @@ namespace PointsBet_Backend_Online_Code_Test
     {
 
         //Code to improve
-        public static string ToCommaSepatatedList(string[] items, string quote)
+        public static string ToCommaSeparatedList(string[] items, string quote)
         {
-            StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
-
-            if (items.Length > 1)
-            {
-                for (int i = 1; i < items.Length; i++)
-                {
-                    qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
-                }
-            }
-
-            return qry.ToString();
+            return String.Join(", ", items.Select(item => $"{quote}{item}{quote}"));
         }
     }
 }
